9840597b49367fbadf059becdee2b53b7b46e17d
Rohan Aggarwal <rohan.aggarwal@thalesgroup.com>
2021-06-27 17:55:36 +0400

    Added sample project

---
+++ Project/prj-monster-01-starting-setup/app.js
@@ -0,0 +1,14 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            playerHealth: 100,
+            monsterHealth: 100
+        };
+    },
+    methods: {
+        
+    }
+});
+
+
+app.mount('#game');
